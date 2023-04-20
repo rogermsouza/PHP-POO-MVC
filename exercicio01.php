948709ef46627ad17e2650f45ee13eeb98539871
rogermsouza <77rogermsouza@gmail.com>
2023-04-20 12:34:33 -0300

    Implementação do exercício 1

---
+++ exercicio01.php
@@ -0,0 +1,7 @@
+<?php
+$separador = "<br><br>----------------------------------------------<br><br>";
+// Exercício 1
+$ex1_a = "ADS é mais legal";
+$ex1_aRet = mb_strtoupper($ex1_a);
+echo $ex1_aRet;
+?>
